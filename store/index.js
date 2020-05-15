a1aaa89a00cce35051f583c95b07a55db0107a4c
unknown <iotbwu0101@gmail.com>
2020-05-15 15:49:14 +0800

    feat: setIsProcessing

---
+++ store/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import app from './modules/app';
+
+Vue.use(Vuex);
+
+const store = () => new Vuex.Store({
+    modules: {
+        app,
+    },
+    getters: {
+        isProcessing: (state) => state.app.isProcessing,
+        processTitle: (state) => state.app.processTitle,
+    },
+});
+
+export default store;
