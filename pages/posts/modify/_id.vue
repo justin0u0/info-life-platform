4ac870ef6cbd16a97cabb52e415072e2c08720ed
陳劭愷 <justin@9dynamics.com.tw>
2020-05-20 23:17:19 +0800

    feat: add tiptap style

---
+++ pages/posts/modify/_id.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="modify-container">
+    <PostsModify />
+  </div>
+</template>
+
+<script>
+import PostsModify from '@/components/posts/Modify';
+
+export default {
+  components: {
+    PostsModify,
+  },
+  asyncData({ params }) {
+    return {
+      id: params.id,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.modify-container {
+  display: flex;
+  justify-content: center;
+  margin-top: 30px;
+}
+</style>
