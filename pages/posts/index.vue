358ace25f3840fbd5e318be1bf2d6c95a3f24fb0
unknown <iotbwu0101@gmail.com>
2020-05-29 15:01:55 +0800

    feat: finish post index v1

---
+++ pages/posts/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Blog />
+  </div>
+</template>
+
+<script>
+import Blog from '@/components/posts/index';
+
+export default {
+  components: {
+    Blog,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
