c1a83e8ac384c84707b0323fef2c8e5bb815b771
unknown <iotbwu0101@gmail.com>
2020-06-25 15:56:54 +0800

    feat: qa-jumbotron

---
+++ components/questions/Index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Jumbotron />
+  </div>
+</template>
+
+<script>
+import Jumbotron from '@/components/questions/Jumbotron';
+
+export default {
+  components: {
+    Jumbotron,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/qa/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <QA />
+  </div>
+</template>
+
+<script>
+import QA from '@/components/questions/Index';
+
+export default {
+  components: {
+    QA,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
