fd60273b85e4042d3384a240e9ef352499fd138a
unknown <iotbwu0101@gmail.com>
2020-06-26 11:46:56 +0800

    feat: view question page

--- components/questions/Jumbotron.vue
+++ components/questions/Jumbotron.vue
@@ -2,9 +2,8 @@
   <div class="jumbotron qa-background">
     <div class="container d-flex align-items-center">
       <div class="qa-total">
-        <!-- TODO: qa-total number-->
-        <p>共<span class="qa-total-number"></span>則提問</p>
-        <p>共<span class="qa-total-number"></span>個回答</p>
+        <p>共<span class="qa-total-number"> {{ totalQuestions }} </span>則提問</p>
+        <p>共<span class="qa-total-number"> {{ totalAnswers }} </span>個回答</p>
       </div>
       <div class="card ml-auto qa-card">
         <div class="card-body">
@@ -13,7 +12,7 @@
             知曉你問題的答案
           </span>
         </div>
-        <div class="card-footer">
+        <div class="card-footer d-flex">
           <div class="w-50 text-center ">
             <a href="#">
               <font-awesome-icon :icon="['fas', 'question']" />
@@ -35,20 +34,43 @@
 </template>
 
 <script>
+import { getQuestions } from '@/api/question';
+
 export default {
   data() {
     return {
+      totalQuestions: 0,
+      totalAnswers: 0,
     };
+  },
+  async mounted() {
+    this.$store.dispatch('setIsProcessing', true);
+    await Promise.all([
+      this.preGetQuestionsTotal(),
+      this.preGetAnswersTotal(),
+    ]);
+    this.$store.dispatch('setIsProcessing', false);
+  },
+  methods: {
+    async preGetQuestionsTotal() {
+      const { total } = await getQuestions({});
+      this.totalQuestions = total;
+    },
+    async preGetAnswersTotal() {
+      const { total } = await getQuestions({ filter: { is_solved: true } });
+      this.totalAnswers = total;
+    },
   },
 };
 </script>
 
 <style scoped>
 .qa-background {
-  background: url('/assets/qa_background.jpg');
+  background-image: url('/assets/qa_background.jpg');
   background-repeat: no-repeat;
   background-position: bottom;
   background-size: cover;
+  font-family: Helvetica, Arial, "LiHei Pro", 黑體-繁, 微軟正黑體, sans-serif;
 }
 .qa-total {
   font-size: 2rem;
---
+++ components/questions/PreviewQuestion.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="row mx-0 mb-4">
+    <div>
+      <h5>{{ questionData.title }}</h5>
+      <div class="d-flex align-items-center">
+        <!-- <div class="qa-tag-list mr-3">
+          <button class="btn btn-sm">選課</button>
+          <button class="btn btn-sm">課業</button>
+        </div> -->
+        <span class="mr-3">{{ transformDate(questionData.created_at) }}</span>
+        <span>
+          由<a href="#"> justin0u0 </a>提問
+        </span>
+      </div>
+    </div>
+    <div class="ml-auto">
+      <div class="row text-center">
+        <!-- TODO: view_count -->
+        <div class="col">
+          <p>瀏覽</p>
+          <span>128</span>
+        </div>
+        <div class="col">
+          <p>回答</p>
+          <span>7</span>
+        </div>
+        <div class="col">
+          <p>喜歡</p>
+          <span>43</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    questionData: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    transformDate(unixEpoch) {
+      const d = new Date(unixEpoch);
+      return d.toLocaleDateString();
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
