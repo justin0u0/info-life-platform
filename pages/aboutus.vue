2e53329290f013ce9be89d5ec344eeab340efd7b
HarryLo <sqaz31117@gapp.nthu.edu.tw>
2020-06-26 13:47:38 +0800

    feature/aboutus: done

---
+++ pages/aboutus.vue
@@ -0,0 +1,22 @@
+<template>
+  <el-container class="justify-content-center">
+    <Aboutus />
+  </el-container>
+</template>
+
+<script>
+import Aboutus from '@/components/Aboutus';
+
+export default {
+  components: {
+    Aboutus,
+  },
+};
+</script>
+
+<style scoped>
+.el-container {
+  margin: 20px 0;
+  text-align: center;
+}
+</style>
