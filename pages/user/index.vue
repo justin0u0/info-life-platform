eebaac5c26ea917ccbf59367e2d41a400602bd9c
陳劭愷 <justin@9dynamics.com.tw>
2020-06-26 21:48:43 +0800

    feat: build AsideNav component

---
+++ pages/user/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-container>
+    <UserAsideNav />
+    <el-main>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+import UserAsideNav from '@/components/user/AsideNav.vue';
+
+export default {
+  components: {
+    UserAsideNav,
+  },
+};
+</script>
+
+<style>
+
+</style>
