c11a4c56f1808b7fe1a060291f39096182ace928
unknown <iotbwu0101@gmail.com>
2020-06-28 22:01:58 +0800

    feat: add modify and published button

---
+++ components/user/PostButton.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <a :href="`/post/modify/${postData._id}`">
+      <el-button type="primary" plain class="mt-lg-2">
+        <div class="d-flex align-items-center">
+          <font-awesome-icon class="icon-size" :icon="['far', 'edit']" />
+          <span class="ml-2">編輯</span>
+        </div>
+      </el-button>
+    </a>
+    <el-button
+      type="success"
+      plain
+      class="mt-2"
+      @click="handleTogglePostIsPublished()"
+    >
+      <div class="d-flex align-items-center">
+        <font-awesome-icon class="icon-size" :icon="['far', 'arrow-alt-circle-up']" />
+        <span class="ml-2">
+          {{ isPublished ? '取消發佈' : '發佈' }}
+        </span>
+      </div>
+    </el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserPostButton',
+  props: {
+    postData: {
+      type: Object,
+      required: true,
+    },
+    isPublished: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  methods: {
+    handleTogglePostIsPublished() {
+      this.$emit('handleTogglePostIsPublished');
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
