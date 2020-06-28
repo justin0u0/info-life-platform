221b9c0634afb43101e5ab46811889f38fc1e1d8
107062107 <lion60609@gmail.com>
2020-06-28 16:15:18 +0800

    c

---
+++ components/TableOfContent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'TableOfContent',
+  props: {
+    contentData: {
+      type: Object,
+      required: true,
+    },
+  },
+  // data() {
+  //   return {
+      
+  //   };
+  // },
+  mounted() {
+    this.fetchHeadingType();
+  },
+  methods: {
+    fetchHeadingType() {
+      console.log(this.contentObj.content.filter((datum) => datum.type === 'heading'));
+    },
+    
+  },
+};
+</script>
+
+<style scoped>
+</style>
