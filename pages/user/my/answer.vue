8f4aa9a932d4245c125896bf0f1737e4ec4b507c
HarryLo <sqaz31117@gapp.nthu.edu.tw>
2020-06-29 13:38:00 +0800

    feature/myAnswer: temp

---
+++ pages/user/my/answer.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-container>
+    <AsideNav />
+    <el-main>
+      <div class="container question-container">
+        <AnswerTab />
+      </div>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+import auth from '@/lib/auth';
+import AsideNav from '@/components/user/AsideNav.vue';
+import AnswerTab from '@/components/user/AnswerTab.vue';
+
+export default {
+  components: {
+    AsideNav,
+    AnswerTab,
+  },
+  mounted() {
+    auth.bind(this)('/');
+  },
+};
+</script>
+
+<style scoped>
+.question-container {
+  max-width: 960px;
+}
+</style>
